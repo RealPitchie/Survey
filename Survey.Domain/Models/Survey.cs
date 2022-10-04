eec6c07cff7ed7c6975ce2a5e93944d1750b9dee
Александр Румянцев <pitchie@list.ru>
2022-10-04 09:28:33 +0300

    initial

---
+++ Survey.Domain/Models/Survey.cs
@@ -0,0 +1,9 @@
+namespace Survey.Domain.Models;
+
+public class Survey
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedAt { get; set; }
+    
+}
