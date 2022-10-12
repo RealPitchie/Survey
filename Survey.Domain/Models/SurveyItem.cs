2d01725bfdd96d9019224337becb4fec46685f63
Александр Румянцев <pitchie@list.ru>
2022-10-12 14:12:07 +0300

    Survey items addind & design stuff

--- Survey.Domain/Models/SurveyItem.cs
+++ Survey.Domain/Models/SurveyItem.cs
@@ -4,6 +4,13 @@ public class SurveyItem
 {
     public string Id { get; set; }
     public string Name { get; set; }
-    public string Type { get; set; }
-    public virtual ICollection<SurveyItemOption> Options { get; set; }
+    public ItemType Type { get; set; }
+    public string Options { get; set; }
+}
+
+public enum ItemType
+{ 
+    Text, 
+    List,
+    YesNo
 }
